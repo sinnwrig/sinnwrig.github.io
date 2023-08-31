433c7e6b78e4a91dcb4cdb4689d6c683cb5207ea
sinnwrig <116330012+sinnwrig@users.noreply.github.com>
2023-08-30 18:02:08 -0700

    Please work

--- JS/Shader.js
+++ JS/Shader.js
@@ -1,8 +1,15 @@
-// Fetch fragment shader
-fetch('https://sinnwrig.github.io/Shaders/Fragment.hlsl').then(response => response.text()).then(function(shader) 
+function LoadFullscreenShader(url)
 {
-    const segments = new URL(response.url).pathname.split('/');
-    const last = segments.pop() || segments.pop();
-    console.log('Loaded shader ' + last);
-    AddFullscreenPlane(shader);
-});
+    fetch(url)
+    .then(response => response.text())
+    .then((shader) => 
+    {
+        const segments = new URL(url).pathname.split('/');
+        const last = segments.pop() || segments.pop();
+        console.log('Loaded shader ' + last);
+        AddFullscreenPlane(shader);
+    });
+}
+
+// Fetch fragment shader
+LoadFullscreenShader('https://sinnwrig.github.io/Shaders/Fragment.hlsl');
