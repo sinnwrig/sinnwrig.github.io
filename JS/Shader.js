c60276ef2308917d247fce658afae9b2b06135b3
sinnwrig <116330012+sinnwrig@users.noreply.github.com>
2023-08-30 17:52:33 -0700

    Goofy

--- JS/Shader.js
+++ JS/Shader.js
@@ -1,7 +1,6 @@
 // Fetch fragment shader
-const response = fetch('https://sinnwrig.github.io/Shaders/Fragment.hlsl');
-
-response.text().then(function(shader) {
+fetch('https://sinnwrig.github.io/Shaders/Fragment.hlsl').then(response => response.text()).then(function(shader) 
+{
     const segments = new URL(response.url).pathname.split('/');
     const last = segments.pop() || segments.pop();
     console.log('Loaded shader ' + last);
